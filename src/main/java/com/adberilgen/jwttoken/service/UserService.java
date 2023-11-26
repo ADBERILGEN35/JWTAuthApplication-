54dce4faefe7e823a32e365e93360cc35df26541
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-11-26 17:16:30 +0300

    JWT Aut || UserService added.

---
+++ src/main/java/com/adberilgen/jwttoken/service/UserService.java
@@ -0,0 +1,49 @@
+package com.adberilgen.jwttoken.service;
+
+import com.adberilgen.jwttoken.dto.CreateUserRequest;
+import com.adberilgen.jwttoken.model.User;
+import com.adberilgen.jwttoken.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Optional<User> getByUserName(String userName) {
+        return userRepository.findByUsername(userName);
+    }
+
+    public User createUser(CreateUserRequest request) {
+        User newUser = User.builder()
+                .name(request.name())
+                .username(request.username())
+                .password(passwordEncoder.encode(request.password()))
+                .authorities(request.authorities())
+                .accountNonExpired(true)
+                .credentialsNonExpired(true)
+                .isEnabled(true)
+                .accountNonLocked(true)
+                .build();
+        return userRepository.save(newUser);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        return user.orElseThrow((EntityNotFoundException::new));
+    }
+
+}
